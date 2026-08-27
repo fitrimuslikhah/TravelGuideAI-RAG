970ebd17641d12c8f650b460b5363cfa35cf5dc2
fitri <fitri@DESKTOP-L1PI6PJ.localdomain>
2026-08-27 20:44:55 +0700

    add terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
